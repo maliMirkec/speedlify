8d65453800653dfbc3593bfb015abe16c60db542
Silvestar Bistrović <malimirkeccita@gmail.com>
2023-05-16 11:13:30 +0200

    Create sb.js

---
+++ _data/sites/sb.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "SB", // optional, falls back to object key
+	description: "My site",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://www.silvestar.codes/"
+	]
+};
