31e0860f98af725578a2e2cb0c6acea895cd1dcb
Silvestar Bistrović <malimirkeccita@gmail.com>
2023-07-07 14:29:23 +0200

    Create sb-staging.js

---
+++ _data/sites/sb-staging.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "SB [Staging]", // optional, falls back to object key
+	description: "My site [staging]",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://staging.silvestar.codes/"
+	]
+};
